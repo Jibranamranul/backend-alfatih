366fd5eb7f9f00a5e023448f5b744504101e4f8a
unknown <Jibranamranul12@gmail.com>
2025-03-16 07:14:01 +0700

    add cors

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000 
+const port = 8000 
 const routes = require("./routes/routes")
 const bodyparser = require("body-parser")
+const cors = require("cors")
+
 
 
 app.use(bodyparser.json())
+app.use(cors())
 
 
 app.get('/news', routes)
 
+
+
 app.post('/news/post', routes)
+
+
 
 app.delete('/news/delete', routes)
 
+
+
 app.get('/image', routes)
+
 
 
 app.listen(port, function(err){
